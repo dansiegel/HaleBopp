3ffaa4e4d8d14a4c4c804efa87606e504a6bf6f6
Dan Siegel <me@dansiegel.net>
2019-12-21 10:40:12 -0800

    Simplifying RootNavigationController

---
+++ src/HaleBopp/Platform/HaleBoppActivity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Comet;
+using Comet.Android;
+using HaleBopp.Common;
+using HaleBopp.Navigation;
+
+namespace HaleBopp.Platform
+{
+    public class HaleBoppActivity : CometActivity, IRootNavigationController
+    {
+        View IRootNavigationController.View
+        {
+            get => Page;
+            set => Page = value;
+        }
+
+        protected void Start<TApp>(string uri, IParameters parameters = null)
+            where TApp : ICometApp =>
+            Start<TApp>(UriParsingHelper.Parse(uri), parameters);
+
+        protected void Start<TApp>(Uri uri, IParameters parameters = null)
+            where TApp : ICometApp
+        {
+            CometHost.Start<TApp>(this, uri, parameters);
+        }
+    }
+}
---
+++ src/HaleBopp/Platform/HaleBoppApplicationDelegate.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Comet;
+using Comet.iOS;
+using HaleBopp.Common;
+using HaleBopp.Navigation;
+using UIKit;
+
+namespace HaleBopp.Platform
+{
+    public abstract class HaleBoppApplicationDelegate : UIApplicationDelegate
+    {
+        //public override UIWindow Window { get; set; }
+
+        UIWindow window;
+
+        protected void Start<TApp>(string uri, IParameters parameters = null)
+            where TApp : ICometApp =>
+            Start<TApp>(UriParsingHelper.Parse(uri), parameters);
+
+        protected void Start<TApp>(Uri uri, IParameters parameters = null)
+            where TApp : ICometApp
+        {
+            var controller = new HaleBoppViewController();
+            CometHost.Start<TApp>(controller, uri, parameters);
+
+            window = new UIWindow(UIScreen.MainScreen.Bounds)
+            {
+                RootViewController = controller
+            };
+            window.MakeKeyAndVisible();
+        }
+    }
+
+    public class HaleBoppViewController : CometViewController, IRootNavigationController
+    {
+        View IRootNavigationController.View
+        {
+            get => CurrentView;
+            set => CurrentView = value;
+        }
+    }
+}
